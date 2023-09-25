5ebac908c71bb3c932ddf3c081371cd54f35e55e
vishwanath <vishwa08patil@gmail.com>
2023-09-25 22:18:42 +0530

    Finding capital letter using if condition

---
+++ Demoif2.java
@@ -0,0 +1,12 @@
+public class Demoif2 {
+  public static void main(String[] args) {
+    System.out.println("Program start");
+    char x = 'W';
+    if (x >= 65 && x <= 91) {
+      System.out.println(x + " is a capital letter");
+    }
+    {
+      System.out.println("End");
+    }
+  }
+}
