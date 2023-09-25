74cab5249fc9469d92d162774bbc48b4dac96c8f
vishwanath <vishwa08patil@gmail.com>
2023-09-25 22:23:58 +0530

    Finding small letter using if condition

---
+++ Demoif3.java
@@ -0,0 +1,8 @@
+public class Demoif3 {
+  public static void main(String[] args) {
+    char x = 'z';
+    if (x >= 'a' && x <= 'z') {
+      System.out.println(x + " is a small letter");
+    }
+  }
+}
